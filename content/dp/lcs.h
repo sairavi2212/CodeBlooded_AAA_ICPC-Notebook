3352725023f79160b1ae9a66010e4091c83e6dfc
adityagauraa@gmail.com <adityagauraa@gmail.com>
2025-11-07 20:12:47 +0530

    added dp and misc

---
+++ content/dp/lcs.h
@@ -0,0 +1,8 @@
+int lcs(string a,string b){
+    int n=a.size(),m=b.size();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;i++)
+     for(int j=1;j<=m;j++)
+      dp[i][j]=a[i-1]==b[j-1]?1+dp[i-1][j-1]:max(dp[i-1][j],dp[i][j-1]);
+    return dp[n][m];
+}
---
+++ content/dp/subset_sum.h
@@ -0,0 +1,7 @@
+int subset_sum(vector<int>& a,int sum){
+    vector<int> dp(sum+1,0); dp[0]=1;
+    for(int x:a)
+      for(int s=sum;s>=x;s--)
+        dp[s]+=dp[s-x];
+    return dp[sum];
+}
